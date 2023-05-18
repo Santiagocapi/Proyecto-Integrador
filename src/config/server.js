adf7fb186b765e7a4f04e533808f196c7426bd7a
Capitani Clavero Santiago <s.capitani@etec.local>
2023-05-18 11:58:26 -0300

    new back-end and database

--- src/config/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-// settings
-app.set(app.set('port'), process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.set('views', path.joint(__dirname, '../app/views'));
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./config/server');
-
-
-// starting the server
-app.listen(app.get('port'), () => {
-     console.log('server on port', app.get('port'));
-})
-
