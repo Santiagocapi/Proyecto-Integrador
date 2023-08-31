a4acca5511079e72bd2fcaee6a1046e68bb8deb8
Santiagocapi <capitanisantiago@gmail.com>
2023-08-30 22:01:33 -0300

    i am using a template engine called ejs to be able to insert the events from the database, i try to insert the events in the ejs, i tried to insert the events into the databse with code it worked because it had an error when reading the database it did not recognize the data type

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000; // Puerto en el que se ejecutará el servidor
+
+// URL de conexión a la base de datos
+const uri = "mongodb+srv://scapitani:capi1234@cluster0.o3moaui.mongodb.net/?retryWrites=true&w=majority"; 
+
+// Establecer ejs como motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/eventos', async (req, res) => {
+  try {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+
+    const database = client.db('nombre_de_la_base_de_datos');
+    const eventosCollection = database.collection('eventos');
+
+    const eventos = await eventosCollection.find({}).toArray();
+    
+    // Renderizar la vista 'eventos.ejs' con los datos de los eventos
+    res.render('eventos', { eventos });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Ocurrió un error en el servidor.' });
+  } 
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
+
+// LEER Y IMPRIMIR LA BASE DE DATOS EN EL FRONT
+// app.get('/eventos', async (req, res) => {
+//   try {
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     await client.connect();
+
+//     const database = client.db('musicEvents');
+//     const eventosCollection = database.collection('Events');
+
+//     const eventos = await eventosCollection.find({}).toArray();
+//     res.json(eventos);
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).json({ message: 'Ocurrió un error en el servidor.' });
+//   } finally {
+//     client.close();
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Servidor escuchando en el puerto ${port}`);
+// });
---
+++ releases.md
@@ -0,0 +1,23 @@
+## [0.0.1] - 2023-08-30
+
+### Added
+
+- Ejs template engine to show the events that are in the database.
+
+### Changed
+
+- Instead of trying to read the database (which had an error when reading because of the type of data it was), I tried to insert the events into the database with code and it worked.
+
+- Musical events.
+
+- Put the styles in the ejs (<styles>).
+
+### Fixed
+
+- Attempt to read the database.
+
+### Unreleased
+
+- Add plays apart from musical events.
+- Delete css files (maybe).
+- Insert the events in the ejs
